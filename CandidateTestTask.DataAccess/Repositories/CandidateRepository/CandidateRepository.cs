e08f60b1b7af208bcc5638d2903e3c08acb26488
Ogabek <hamzaqulovogabek@gmail.com>
2024-10-03 16:56:10 +0500

    Added Candidate Repository

---
+++ CandidateTestTask.DataAccess/Repositories/CandidateRepository/CandidateRepository.cs
@@ -0,0 +1,43 @@
+﻿using CandidateTestTask.DataAccess.Context;
+using CandidateTestTask.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CandidateTestTask.DataAccess.Repositories.CandidateRepository;
+
+public class CandidateRepository : ICandidateRepository
+{
+    private readonly AppDbContext _context;
+
+    public CandidateRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<int> AddCandidateAsync(Candidate candidate)
+    {
+        await _context.AddAsync(candidate);
+        await _context.SaveChangesAsync();
+
+        return candidate.Id;
+    }
+    public async Task<int> UpdateCandidateAsync(Candidate candidate)
+    {
+        _context.Update(candidate);
+        await _context.SaveChangesAsync();
+
+        return candidate.Id;
+    }
+    public async Task<Candidate?> GetCandidateByEmail(string email)
+    {
+        return await _context.Candidates.FirstOrDefaultAsync(c => c.Email == email);
+    }
+    public async Task<Candidate?> GetCandidateByIdAsync(int id)
+    {
+        return await _context.Candidates.FindAsync(id);
+    }
+    public IQueryable<Candidate> GetAllCandidates()
+    {
+        return _context.Candidates;
+    }
+
+}
---
+++ CandidateTestTask.DataAccess/Repositories/CandidateRepository/ICandidateRepository.cs
@@ -0,0 +1,13 @@
+﻿using CandidateTestTask.Domain.Models;
+
+namespace CandidateTestTask.DataAccess.Repositories.CandidateRepository
+{
+    public interface ICandidateRepository
+    {
+        Task<int> AddCandidateAsync(Candidate candidate);
+        IQueryable<Candidate> GetAllCandidates();
+        Task<Candidate?> GetCandidateByEmail(string email);
+        Task<Candidate?> GetCandidateByIdAsync(int id);
+        Task<int> UpdateCandidateAsync(Candidate candidate);
+    }
+}
