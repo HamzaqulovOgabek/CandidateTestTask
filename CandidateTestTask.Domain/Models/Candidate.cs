51228c8fed5a1b73b6b5ecef607cac57dc6a8434
Ogabek <hamzaqulovogabek@gmail.com>
2024-10-03 12:41:39 +0500

    Added Candidate Model

---
+++ CandidateTestTask.Domain/Models/Candidate.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CandidateTestTask.Domain.Models;
+
+[Table(nameof(Candidate))]
+public class Candidate
+{
+    [Key]
+    public required int Id { get; set; }
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public string? PhoneNumber { get; set; }
+    public required string Email { get; set; }
+    public string? LinkedInUrl { get; set; }
+    public string? GitHubUrl { get; set; }
+    public string? Comment { get; set; }
+
+}
