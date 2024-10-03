b24c7c5933fbe55a6c080caccecc921a8a2d28da
Ogabek <hamzaqulovogabek@gmail.com>
2024-10-03 16:57:38 +0500

    Used Automapper

---
+++ CandidateTestTask.Application/Services/CandidateServices/Dto/CandidateMapperProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using CandidateTestTask.Domain.Models;
+
+namespace CandidateTestTask.Application.Services.CandidateServices;
+
+public class CandidateMapperProfile : Profile
+{
+    public CandidateMapperProfile()
+    {
+        CreateMap<CandidateCreateDto, Candidate>();
+    }
+}
