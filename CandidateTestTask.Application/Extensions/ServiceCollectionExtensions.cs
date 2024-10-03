1e15f5077a09150044c0f5009187c98c2312f337
Ogabek <hamzaqulovogabek@gmail.com>
2024-10-03 16:59:53 +0500

    Added BaseSortFilterDto

--- CandidateTestTask.Application/Extensions/ServiceCollectionExtensions.cs
+++ CandidateTestTask.Application/Extensions/ServiceCollectionExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using CandidateTestTask.Application.Services.CandidateServices;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace CandidateTestTask.Application.Extensions;
 
@@ -8,6 +9,8 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddApplication(this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
         services.AddScoped<ICandidateService, CandidateService>();
 
         return services;
