21110c3fe3430d33228f392bd89f849f23b180e1
Ogabek <hamzaqulovogabek@gmail.com>
2024-10-03 16:57:56 +0500

    Added Validation Attribute

---
+++ CandidateTestTask.Application/Services/CandidateServices/Dto/ValidationAttributes/EmailValidationAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CandidateTestTask.Application.Services.CandidateServices;
+
+public class EmailValidationAttribute : ValidationAttribute
+{
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        var dto = validationContext.ObjectInstance as CandidateCreateDto;
+
+        if (dto == null ||
+            string.IsNullOrWhiteSpace(dto.Email) ||
+            !dto.IsValidEmail(dto.Email)
+        )
+        {
+            return new ValidationResult("Invalid Email");
+        }
+
+        return ValidationResult.Success;
+    }
+}
