6c0e91d2f014357afc452f2d8c6164522339ef14
Ogabek <hamzaqulovogabek@gmail.com>
2024-10-03 12:43:36 +0500

    Added DbContext

---
+++ CandidateTestTask.DataAccess/Context/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using CandidateTestTask.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CandidateTestTask.DataAccess.Context;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Candidate> Candidates { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Candidate>(options =>
+        {
+            options.HasIndex(c => c.Email)
+            .IsUnique();
+        });
+    }
+}
