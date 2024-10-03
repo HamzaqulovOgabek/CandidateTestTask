8919f20136e8c713c0a677f15aafb8ea3e45e8b5
Ogabek <hamzaqulovogabek@gmail.com>
2024-10-03 17:00:09 +0500

    Added Custom Exception

---
+++ CandidateTestTask.Application/Exceptions/CandidateNotFoundException.cs
@@ -0,0 +1,8 @@
+﻿namespace CandidateTestTask.Application.Exceptions;
+
+public class CandidateNotFoundException : Exception
+{
+    public CandidateNotFoundException(string message) : base(message)
+    {
+    }
+}
