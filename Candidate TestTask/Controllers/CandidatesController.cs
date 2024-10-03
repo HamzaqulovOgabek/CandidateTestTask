7fad9ea4d56b33a0a75300f2fdee1c61c5d760a5
Ogabek <hamzaqulovogabek@gmail.com>
2024-10-03 17:00:59 +0500

    Added Candidate Controller

---
+++ Candidate TestTask/Controllers/CandidatesController.cs
@@ -0,0 +1,40 @@
+﻿using CandidateTestTask.Application.Services.CandidateServices;
+using CandidateTestTask.Domain.Models;
+using Market.Auth.Application.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Candidate_TestTask.Controllers;
+
+[ApiController]
+[Route("[controller]/[action]")]
+public class CandidatesController : ControllerBase
+{
+    private readonly ICandidateService _service;
+
+    public CandidatesController(ICandidateService service)
+    {
+        _service = service;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateAsync(CandidateCreateDto dto)
+    {
+        return Ok(await _service.AddOrUpdateCandidateAsync(dto));
+    }
+    [HttpPost]
+    public async Task<IActionResult> UpdateAsync(CandidateUpdateDto dto)
+    {
+        return Ok(await _service.UpdateCandidateAsync(dto));
+    }
+    [HttpGet("{id}")]
+    public async Task<IActionResult> Get(int id)
+    {
+        return Ok(await _service.GetCandidate(id));
+    }
+    [HttpPost]
+    public IActionResult GetList(BaseSortFilterDto options)
+    {
+        return Ok(_service.GetAllCandidates(options));
+    }
+
+}
