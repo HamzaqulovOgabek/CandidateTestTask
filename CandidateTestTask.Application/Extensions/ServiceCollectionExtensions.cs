268a060ace0fea246edf03c18e7c859e8a8331ee
Ogabek <hamzaqulovogabek@gmail.com>
2024-10-03 14:40:59 +0500

    Added Service Collection Extension for Repository and Application

---
+++ CandidateTestTask.Application/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using CandidateTestTask.Application.Services.CandidateServices;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CandidateTestTask.Application.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddScoped<ICandidateService, CandidateService>();
+
+        return services;
+    }
+}
---
+++ CandidateTestTask.DataAccess/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using CandidateTestTask.DataAccess.Context;
+using CandidateTestTask.DataAccess.Repositories.CandidateRepository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CandidateTestTask.DataAccess.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddDataAccess(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("Candidates"));
+        });
+
+        services.AddScoped<ICandidateRepository, CandidateRepository>();
+
+        return services;
+    }
+}
